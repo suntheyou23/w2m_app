495661406078f0145ac71a90ddd6c7c9d2e9f3fb
suntheyou23 <g27152@ktc.ac.jp>
2020-05-06 02:03:48 +0900

    first commit

---
+++ js/w2m.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".change").on("click", function() {
+        var w, m = "";
+
+        w = $("#w_box").val();
+
+        for (var i=0; i<(w.match( /w/g ) || [] ).length; i++) {
+            m += 'm';
+        }
+
+
+    
+        $("#w_box").val(m);
+    }); 
+});
